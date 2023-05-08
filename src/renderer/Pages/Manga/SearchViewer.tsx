1b8404d14d79162025158977db57cfdc04e15459
wyt05 <90769809+wyt05@users.noreply.github.com>
2023-05-08 17:51:43 +0800

    Manga Search Stuff

--- src/renderer/Pages/Manga/SearchViewer.tsx
+++ src/renderer/Pages/Manga/SearchViewer.tsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import { DefaultNewButton } from "../../Utils/componentStyle";
+import MangaCard from "../../UiComponents/MangaCard";
+import qs from "qs";
 
 //We will use consumet for retrieving Manga chapters as it's more straightforward
 //But we will use MangaDEX API for search as it's more robust compared to consumet.
@@ -10,9 +12,12 @@ import { DefaultNewButton } from "../../Utils/componentStyle";
 function MangaSearchViewer() {
   const [searchManga, setSearchManga] = React.useState("");
   const [searchReturnList, setSearchReturnList] = React.useState<Array<any>>();
-  const [totalNumPages, setTotalNumPages] = React.useState(1)
+  const [totalNumPages, setTotalNumPages] = React.useState(1);
   const [currentMangaPage, setCurrentMangaPage] = React.useState(1);
   const [maxMangaPages, setMaxMangaPages] = React.useState(1);
+
+  const [openMangaDialog, setOpenMangaDialog] = React.useState(false);
+  const [selectedMangaID, setSelectedMangaID] = React.useState<string>("");
 
   //OFFSEt
 
@@ -24,24 +29,34 @@ function MangaSearchViewer() {
       includedTagsMode: "AND",
       excludedTagsMode: "OR",
       contentRating: ["safe", "suggestive", "erotica"],
+      includes: ["cover_art"],
       order: {
         latestUploadedChapter: "desc",
       },
     };
-    axios.get("https://api.mangadex.org/manga", {params})
-    .then((response)=> {
-        setSearchReturnList(response.data.data)
-        let calcPages = response.data.total / response.data.limit
-        let actualPages = Math.ceil(calcPages)
-        setTotalNumPages(actualPages)
-    })
+    axios
+      .get("https://api.mangadex.org/manga", {
+        params,
+        paramsSerializer: (params) =>
+          qs.stringify(params, {
+            arrayFormat: "brackets",
+            encodeValuesOnly: true,
+          }),
+      })
+      .then((response) => {
+        setSearchReturnList(response.data.data);
+        console.log(response.data.data)
+        let calcPages = response.data.total / response.data.limit;
+        let actualPages = Math.ceil(calcPages);
+        setTotalNumPages(actualPages);
+      });
   }
 
   return (
     <>
-      <Box sx={{display: "flex"}}>
+      <Box sx={{ display: "flex" }}>
         <TextField
-            sx={{flexGrow: 1}}
+          sx={{ flexGrow: 1 }}
           id="search-anime"
           label="Search for Manga"
           value={searchManga}
@@ -49,11 +64,24 @@ function MangaSearchViewer() {
             setSearchManga(e.target.value);
           }}
         />
-        <DefaultNewButton sx={{flexGrow: 0.2, marginLeft: "7px"}} variant="contained">
-                Search
+        <DefaultNewButton
+          sx={{ flexGrow: 0.2, marginLeft: "7px" }}
+          variant="contained"
+          onClick={searchMangaOnClick}
+        >
+          Search
         </DefaultNewButton>
       </Box>
-      
+
+      <Box sx={{ display: "flex", flexFlow: "wrap", padding: "5px" }}>
+        {searchReturnList?.map((obj, i) => (
+          <MangaCard
+            mangaObj={obj}
+            setSelectedID={setSelectedMangaID}
+            setOpenDialog={setOpenMangaDialog}
+          />
+        ))}
+      </Box>
     </>
   );
 }
--- src/renderer/UiComponents/MangaCard/index.tsx
+++ src/renderer/UiComponents/MangaCard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -11,18 +11,73 @@ import { CardActions } from "@mui/material";
 import Button from "@mui/material/Button";
 
 interface IProps {
-    mangaObj: {
-        [key: string]: any;
-    };
-    setOpenDialog: any;
-    setSelectedID: any;
+  mangaObj: {
+    [key: string]: any;
+  };
+  setOpenDialog: any;
+  setSelectedID: any;
 }
 
-function MangaCard(props: IProps){
-    const {mangaObj, setSelectedID, setOpenDialog} = props;
-    
-    <Card>
-        
+function MangaCard(props: IProps) {
+  const { mangaObj, setSelectedID, setOpenDialog } = props;
+  const { id, type, attributes, relationships } = mangaObj;
+
+  //Main attributes
+  const titleEN = attributes.title.en;
+  const titleJP = attributes.title.jp;
+  const description = attributes.description.en;
+  const themesRAW = attributes.tags;
+
+  const themesCleaned = [];
+
+  for (let item of themesRAW) {
+    themesCleaned.push(item.attributes.name.en);
+  }
+
+  console.log(themesCleaned);
+
+  //COVER ID
+
+  const filteredContentCoverObj = relationships.filter(function (el: any) {
+    return el.type === "cover_art";
+  });
+
+  const coverArtId = filteredContentCoverObj[0].attributes.fileName;
+
+  console.log(description);
+  console.log(titleEN);
+
+  const deriveCoverContent = `https://mangadex.org/covers/${id}/${coverArtId}.512.jpg`;
+
+  return (
+    <Card sx={{ display: "flex", width: "100%", marginTop: "2px", marginBottom: "2px", alignItems: "space-between"}}>
+      <CardMedia
+        component="img"
+        sx={{ width: "215px", objectFit: "cover" }}
+        image={deriveCoverContent}
+        alt={titleEN}
+      />
+      <Box sx={{ display: "flex", flexDirection: "column", alignItems: "space-between" }}>
+        <CardContent sx={{ alignItems: "space-between" }}>
+          <Typography component="div" variant="h4" sx={{marginBottom: "5px"}}>
+            {titleEN}
+          </Typography>
+          {themesCleaned.map((str, i) => (
+            <Chip
+              key={i}
+              label={str}
+              variant="outlined"
+              size="small"
+              sx={{ backgroundColor: "#29AB87" }}
+            />
+          ))}
+          <Typography sx={{marginTop: "20px"}}>
+            {description}
+          </Typography>
+        </CardContent>
+      </Box>
     </Card>
-
+  );
 }
+
+export default MangaCard;
